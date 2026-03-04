4e8e54ba8f9a8c9db533ac977cde128e8e091af8
jlarizza-culmina <jlarizza@culmina.io>
2026-03-03 20:47:06 -0500

    fix: merge vite config exports

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 import tailwindcss from '@tailwindcss/vite'
 
-export default {
+export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
-  ]
-}
+  ],
+})
